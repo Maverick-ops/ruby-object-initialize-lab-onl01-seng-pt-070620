3cc04d3b01441e353662d4030f33c7e059deea93
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-22 22:49:34 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog 
+  def initialize(name, breed)
+    @name = name
+    @breed = breed 
+  if breed = nil 
+    puts "Mutt"
+  end
+end
+  def name=(name)
+    @name = name 
+  end
+  
+  def name
+    @name
+  end
+
+  def breed=(breed)
+   @breed = breed 
+  end 
+ 
+ def breed 
+  @breed
+  end
+  
+end
+ 
